a025e946d4076bed96afa4e7be95656bc1ef8661
cstanescu123 <94859191+cstanescu123@users.noreply.github.com>
2022-05-17 15:02:50 -0400

    Entity Framework Database Created

---
+++ AskCletus_BackEnd/Services/DALModels/DrinkHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AskCletus_BackEnd.Services.DALModels
+{
+    public class DrinkHistory
+    {
+        [Key]
+        public int HistoryId { get; set; }
+        public string DrinkName { get; set; }
+        public DateTime Date { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+    }
+}
