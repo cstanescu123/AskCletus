86e00e21cdcd19ddece8a07962278688a915a2e2
cstanescu123 <94859191+cstanescu123@users.noreply.github.com>
2022-05-23 19:33:30 -0400

    edited user components

--- AskCletus-FrontEnd/src/app/app.module.ts
+++ AskCletus-FrontEnd/src/app/app.module.ts
@@ -7,12 +7,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarHomeComponent } from './userBar/bar-home/bar-home.component';
 import { AddIngredientComponent } from './userBar/add-ingredient/add-ingredient.component';
-import { UserComponent } from './user/user.component';
-import { UserHomeComponent } from './user/user-home/user-home.component';
 import { UpdateUserComponent } from './user/update-user/update-user.component';
 import { DeleteUserComponent } from './user/delete-user/delete-user.component';
-import { GetUserComponent } from './user/get-user/get-user.component';
-import { GetUsersComponent } from './user/get-users/get-users.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
 
 
 @NgModule({
@@ -20,12 +17,9 @@ import { GetUsersComponent } from './user/get-users/get-users.component';
     AppComponent,
     BarHomeComponent,
     AddIngredientComponent,
-    UserComponent,
-    UserHomeComponent,
     UpdateUserComponent,
     DeleteUserComponent,
-    GetUserComponent,
-    GetUsersComponent,
+    UserLoginComponent,
   ],
   imports: [
     BrowserModule,
