a4c6f4de4a3ab3caeae6509ade6b7aa631eab3b3
Kyle McIntosh <krmcintosh87@gmail.com>
2022-05-23 19:32:49 -0400

    Merge branch 'main' of https://github.com/MortalAmongGods/AskCletus into Kyle

--- AskCletus-FrontEnd/src/app/app.module.ts
+++ AskCletus-FrontEnd/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarHomeComponent } from './userBar/bar-home/bar-home.component';
 import { AddIngredientComponent } from './userBar/add-ingredient/add-ingredient.component';
-import { UserComponent } from './user/user.component';
 import { UserHomeComponent } from './user/user-home/user-home.component';
 import { UpdateUserComponent } from './user/update-user/update-user.component';
 import { DeleteUserComponent } from './user/delete-user/delete-user.component';
@@ -20,12 +19,8 @@ import { GetUsersComponent } from './user/get-users/get-users.component';
     AppComponent,
     BarHomeComponent,
     AddIngredientComponent,
-    UserComponent,
-    UserHomeComponent,
     UpdateUserComponent,
     DeleteUserComponent,
-    GetUserComponent,
-    GetUsersComponent,
   ],
   imports: [
     BrowserModule,
