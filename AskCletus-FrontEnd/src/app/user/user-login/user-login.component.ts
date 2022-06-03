061b357c4528405283212073cba93290d41ffd67
corn <corneliusemail@gmail.com>
2022-06-02 20:01:16 -0400

    random stuff, nothing big

--- AskCletus-FrontEnd/src/app/user/user-login/user-login.component.ts
+++ AskCletus-FrontEnd/src/app/user/user-login/user-login.component.ts
@@ -23,7 +23,7 @@ export class UserLoginComponent implements OnInit {
 
 
   ngOnInit(): void {
-    // localhost:4200/?code=12345&state=123435
+    
     this._activatedRoute.queryParams.pipe(
       map(params => ({code: params["code"], state: params["state"]})),
       filter(p => p.code && p.state && p.state === localStorage.getItem("authState")),
