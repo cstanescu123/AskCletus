7d63754ad7d180e4468c36d3598f142cb44b9b6e
MortalAmongGods <krmcintosh87@gmail.com>
2022-06-04 17:03:11 -0400

    testing getting userId

--- AskCletus-FrontEnd/src/app/userBar/bar-home/bar-home.component.ts
+++ AskCletus-FrontEnd/src/app/userBar/bar-home/bar-home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { filter, mergeMap, switchMap } from 'rxjs';
+import { filter, map, mergeMap, switchMap } from 'rxjs';
+import { User } from 'src/app/models/GithubUser';
 import { IngredientsResponse } from 'src/app/models/IngredientsResponse';
 import { AuthService } from 'src/app/Services/auth.service';
 import { UserBarServiceService } from '../../Services/user-bar-service.service';
@@ -11,36 +12,33 @@ import { UserBarServiceService } from '../../Services/user-bar-service.service';
 })
 export class BarHomeComponent implements OnInit {
   
-  bars: IngredientsResponse[] = []
-
   
   constructor(private _userBarService: UserBarServiceService,
     private _authService: AuthService) { }
-    
-    //grab user to get userId to get their version of the bar
-    //if no user, don't stay on this page
-    //if user get userId for user bar
-
-    
-    userLoggedIn$ = .pipe(
+        
+        //grab user to get userId to get their version of the bar
+        //if no user, don't stay on this page
+        //if user get userId for user bar
+        
+    bars: IngredientsResponse[] = []
+    userBar$ = this._authService.user$.pipe(
       filter(x => x !== null),
+      map(x => x!.userId),
       mergeMap(x => this._userBarService.getUserBar(x))
-      );
-
-    ngOnInit(): void {
-    // this._userBarService.getUserBars().subscribe(bars => {
-    //   this.bars = bars;
-    // })
-  }
-
-  getUserBar(userId: number) {}
-
-  removeIngredient(id: number) {
-    const userBars$ = this._userBarService.deleteIngredient(id).pipe(
+    );
+ 
+    removeIngredient(id: number) {
+      const userBars$ = this._userBarService.deleteIngredient(id).pipe(
       switchMap(() => this._userBarService.getUserBars())
       );
       userBars$.subscribe(ingredient => {
       this.bars = ingredient
     });
-  }
+    }
+        
+    ngOnInit(): void {
+    // this._userBarService.getUserBars().subscribe(bars => {
+    //   this.bars = bars;
+    // })
+    }
 }
