5ca453d29c4cd8c810dad3bf844b9bbc53e4a97a
Kyle <90775144+MortalAmongGods@users.noreply.github.com>
2022-06-05 08:40:49 -0400

    Revert "Revert "Kyle""

--- AskCletus-FrontEnd/src/app/userBar/add-ingredient/add-ingredient.component.ts
+++ AskCletus-FrontEnd/src/app/userBar/add-ingredient/add-ingredient.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { filter, map, mergeMap, switchMap } from 'rxjs';
+import { filter, map, mergeMap, Observable, switchMap } from 'rxjs';
 import { PostBar } from 'src/app/models/IngredientsResponse';
 import { AuthService } from 'src/app/Services/auth.service';
 import { UserBarServiceService } from 'src/app/Services/user-bar-service.service';
@@ -9,30 +9,50 @@ import { UserBarServiceService } from 'src/app/Services/user-bar-service.service
 @Component({
   selector: 'app-add-ingredient',
   templateUrl: './add-ingredient.component.html',
-  styleUrls: ['./add-ingredient.component.css']
+  styleUrls: ['./add-ingredient.component.css'],
 })
 export class AddIngredientComponent implements OnInit {
+  constructor(
+    private _userBarService: UserBarServiceService,
+    private _authService: AuthService
+  ) {}
 
-  constructor(private _userBarService: UserBarServiceService,
-              private _authService: AuthService) { }
-
-    addIngredientFormGroup = new FormGroup({
+  addIngredientFormGroup = new FormGroup({
     ingredient: new FormControl(''),
-    userId: new FormControl(''),
-  })
+  });
 
   userBar$ = this._authService.user$.pipe(
-    filter(x => x !== null),
-    map(x => x!.userId),
-    mergeMap(x => this._userBarService.getUserBar(x))
+    filter((x) => x !== null),
+    map((x) => x!.userId)
   );
-
-  userId = this.userBar$
   
+    //3 switchmaps to pipe to http request with form/id
+    //click button = event
+    //grab user info
+    //grab form info
+    //send to post
+@ViewChild('button')
+getIngredientButton!: ElementRef<HTMLButtonElement>
+
+click$!: Observable<any>;
+addIngredientClick$!: Observable<any>;
+
+// ngAfterViewInit(): void {
+//   this.click$ = fromEvent(this.getDogButton.nativeElement, 'click');
+
+//   this.addIngredientClick$ = this.click$.pipe(
+//     mergeMap(_ => this._dogService.getRandomDog()),
+//     map(dogResponse => dogResponse.message)
+//   );
+// }
+
+
+
   submitIngredient() {
     const postBar: PostBar = this.addIngredientFormGroup.value;
+  //  this.userBar$;
     this._userBarService.postIngredient(postBar).subscribe();
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 }
