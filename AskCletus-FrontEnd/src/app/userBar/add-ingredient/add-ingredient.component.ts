efc1b5f8b2d78a9e49f28786bd63c754f59b4030
MortalAmongGods <krmcintosh87@gmail.com>
2022-06-05 08:41:23 -0400

    cleaning

--- AskCletus-FrontEnd/src/app/userBar/add-ingredient/add-ingredient.component.ts
+++ AskCletus-FrontEnd/src/app/userBar/add-ingredient/add-ingredient.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-<<<<<<< HEAD
 import { filter, fromEvent, map, mergeMap, Observable, switchMap } from 'rxjs';
-=======
-import { filter, map, mergeMap, switchMap } from 'rxjs';
->>>>>>> aa5aa601a50fff1fcef153b34f37a7b2342fb806
 import { PostBar } from 'src/app/models/IngredientsResponse';
 import { AuthService } from 'src/app/Services/auth.service';
 import { UserBarServiceService } from 'src/app/Services/user-bar-service.service';
@@ -13,39 +9,47 @@ import { UserBarServiceService } from 'src/app/Services/user-bar-service.service
 @Component({
   selector: 'app-add-ingredient',
   templateUrl: './add-ingredient.component.html',
-  styleUrls: ['./add-ingredient.component.css']
+  styleUrls: ['./add-ingredient.component.css'],
 })
 export class AddIngredientComponent implements OnInit {
-<<<<<<< HEAD
   constructor(
     private _userBarService: UserBarServiceService,
     private _authService: AuthService
-  ) { }
-=======
->>>>>>> aa5aa601a50fff1fcef153b34f37a7b2342fb806
+  ) {}
 
-  constructor(private _userBarService: UserBarServiceService,
-              private _authService: AuthService) { }
-
-    addIngredientFormGroup = new FormGroup({
+  addIngredientFormGroup = new FormGroup({
     ingredient: new FormControl(''),
     userId: new FormControl(''),
-  })
+  });
 
   addIngredient$ = this.addIngredientFormGroup.valueChanges;
 
   userBar$ = this._authService.user$.pipe(
-    filter(x => x !== null),
-    map(x => x!.userId),
-    mergeMap(x => this._userBarService.getUserBar(x))
+    filter((x) => x !== null),
+    map((x) => x!.userId),
+    mergeMap((x) => this._userBarService.getUserBar(x))
   );
 
-  userId = this.userBar$
-  
+  userId = this.userBar$;
+
+  @ViewChild('button')
+  getIngredientButton!: ElementRef<HTMLButtonElement>;
+
+  click$!: Observable<any>;
+  addIngredientClick$!: Observable<any>;
+
   submitIngredient() {
     const postBar: PostBar = this.addIngredientFormGroup.value;
     this._userBarService.postIngredient(postBar).subscribe();
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.click$ = fromEvent(this.getIngredientButton.nativeElement, 'click');
+    this.addIngredientClick$ = this.click$.pipe(
+      mergeMap((_) => this.userBar$),
+      mergeMap((_) => this.addIngredient$)
+    );
+  }
 }
