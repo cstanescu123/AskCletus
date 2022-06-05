386944259dd92b3f8fe5f032a82f2eae599271e6
corn <corneliusemail@gmail.com>
2022-06-05 15:31:47 -0400

    cleaning up

--- AskCletus-FrontEnd/src/app/get-drink-name/get-drink-name.component.ts
+++ AskCletus-FrontEnd/src/app/get-drink-name/get-drink-name.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
-import { DrinkResponse } from '../models/DrinkResponse';
-import { DrinkServiceService } from '../Services/drink-service.service';
 
 @Component({
   selector: 'app-get-drink-name',
@@ -10,10 +7,7 @@ import { DrinkServiceService } from '../Services/drink-service.service';
 })
 export class GetDrinkNameComponent implements OnInit {
 
-
-  constructor(private _drinkService : DrinkServiceService) { 
-
-  }
+  constructor() { }
 
   ngOnInit(): void {
   }
